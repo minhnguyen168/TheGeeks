07e3852e330624315ea99a25594ce15611ad684d
cwk30 <78654405+minhnguyen168@users.noreply.github.com>
2022-08-10 00:49:14 +0800

    fixing version errors for import modules

--- app/forms.py
+++ app/forms.py
@@ -1,21 +1,21 @@
 from flask_wtf import FlaskForm 
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, RadioField, SelectField
-from wtforms.fields.html5 import DateField
-from wtforms.validators import Required, Length, Email, EqualTo, ValidationError, DataRequired
+from wtforms.fields import DateField
+from wtforms.validators import InputRequired, Length, Email, EqualTo, ValidationError, DataRequired
 from app.models import (User)
 from flask_login import current_user
 
 
 class ClientRegistrationForm(FlaskForm):
-    name =  StringField("Name", validators=[Required(), Length(min=2, max=20)])
+    name =  StringField("Name", validators=[InputRequired(), Length(min=2, max=20)])
     #name =  StringField("Name", validators=[Required(), Length(min=1, max=40)]) 
-    nric = StringField("NRIC", validators=[Required(), Length(min=2, max=20)]) 
-    email = StringField('Email', validators=[Required(), Email()]) 
-    password = PasswordField('Password', validators=[Required()])
+    nric = StringField("NRIC", validators=[InputRequired(), Length(min=2, max=20)]) 
+    email = StringField('Email', validators=[InputRequired(), Email()]) 
+    password = PasswordField('Password', validators=[InputRequired()])
     #contactno = StringField('Contact No.', validators=[Required(), Length(min=8, max=8)]) 
 
-    confirm_password = PasswordField('Confirm Password', validators=[Required(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign Up') 
 
@@ -30,9 +30,9 @@ class ClientRegistrationForm(FlaskForm):
             raise ValidationError('That email is already registered. Please login with your registered account.')
 
 class ClientLoginForm(FlaskForm):
-    email =  StringField("Email", validators=[Required(), Email()]) 
+    email =  StringField("Email", validators=[InputRequired(), Email()]) 
 
-    password = PasswordField('Password', validators=[Required()])
+    password = PasswordField('Password', validators=[InputRequired()])
 
     remember = BooleanField('Remember Me')
 
@@ -47,14 +47,14 @@ class ClientLoginForm(FlaskForm):
 #     submit = SubmitField('Update') 
 
 class BankerRegistrationForm(FlaskForm):
-    name =  StringField("Name", validators=[Required(), Length(min=2, max=20)])
+    name =  StringField("Name", validators=[InputRequired(), Length(min=2, max=20)])
     #name =  StringField("Name", validators=[Required(), Length(min=1, max=40)]) 
-    nric = StringField("NRIC", validators=[Required(), Length(min=2, max=20)]) 
-    email = StringField('Email', validators=[Required(), Email()]) 
-    password = PasswordField('Password', validators=[Required()])
+    nric = StringField("NRIC", validators=[InputRequired(), Length(min=2, max=20)]) 
+    email = StringField('Email', validators=[InputRequired(), Email()]) 
+    password = PasswordField('Password', validators=[InputRequired()])
     #contactno = StringField('Contact No.', validators=[Required(), Length(min=8, max=8)]) 
 
-    confirm_password = PasswordField('Confirm Password', validators=[Required(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign Up') 
     
@@ -64,9 +64,9 @@ class BankerRegistrationForm(FlaskForm):
             raise ValidationError('That email is already registered. Please login with your registered account.')
 
 class BankerLoginForm(FlaskForm):
-    email =  StringField("Email", validators=[Required(), Email()]) 
+    email =  StringField("Email", validators=[InputRequired(), Email()]) 
 
-    password = PasswordField('Password', validators=[Required()])
+    password = PasswordField('Password', validators=[InputRequired()])
 
     remember = BooleanField('Remember Me')
 
